58fd5a27aa78a79191e2a8c99e398cbe4ccea2b3
saliktariq <saliktariq@icloud.com>
2019-11-09 01:32:30 +0000

    testing_importing_data

---
+++ testingonly_ignore.R
@@ -0,0 +1,4 @@
+training_data <- read.table("Training_set for Q5.txt", header = TRUE, sep = ",")
+testing_data <- read.table("Testing_set for Q5.txt", header = TRUE, sep = ",")
+head(training_data)
+head(testing_data)
